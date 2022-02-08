dc57e1bc86c40325465503b5ad687b32267b830a
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-08 15:49:36 -0600

    new

--- Navbar-1/Navbar-1.js
+++ Navbar-1/Navbar-1.js
@@ -1,2 +1,9 @@
-/*This navbar needs Bootstrap*/
 
+window.onscroll = function () {navChangeOnScroll()}
+function navChangeOnScroll() {
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById('navbar').classList.add('active_navbar');
+    } else {
+        document.getElementById('navbar').classList.remove('active_navbar');
+    }
+}
